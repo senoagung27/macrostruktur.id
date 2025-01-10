8b96a3892ebbace034d219d4c1de1fe9acd89396
Nada <nada@nusanetwork.com>
2025-01-10 08:16:05 +0700

    update

--- resources/views/frontend/layout/head.blade.php
+++ resources/views/frontend/layout/head.blade.php
@@ -32,14 +32,14 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets_2/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets_2/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets_2/assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets_2/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets_2/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
     <!-- Main CSS File -->
-    <link href="assets_2/assets/css/main.css" rel="stylesheet">
+    <link href="{{ asset('assets_2/assets/css/main.css') }}" rel="stylesheet">
 
     <style>
         /* Gambar Full Height */
@@ -88,16 +88,16 @@
 @include('frontend.layout.footer')
 
  <!-- Vendor JS Files -->
- <script src="assets_2/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
- <script src="assets_2/assets/vendor/php-email-form/validate.js"></script>
- <script src="assets_2/assets/vendor/aos/aos.js"></script>
- <script src="assets_2/assets/vendor/glightbox/js/glightbox.min.js"></script>
- <script src="assets_2/assets/vendor/waypoints/noframework.waypoints.js"></script>
- <script src="assets_2/assets/vendor/purecounter/purecounter_vanilla.js"></script>
- <script src="assets_2/assets/vendor/swiper/swiper-bundle.min.js"></script>
- <script src="assets_2/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
- <script src="assets_2/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+ <script src="{{ asset('assets_2/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/php-email-form/validate.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/aos/aos.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+ <script src="{{ asset('assets_2/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
 
  <!-- Main JS File -->
- <script src="assets_2/assets/js/main.js"></script>
+ <script src="{{ asset('assets_2/assets/js/main.js') }}"></script>
 </html>
