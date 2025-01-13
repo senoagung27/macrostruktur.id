6f5594c3b36eebc42fe14555619028aa058566d4
Nada <nada@nusanetwork.com>
2025-01-13 09:09:35 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -81,10 +81,10 @@ Route::group(['middleware' => 'auth'], function(){
     Route::resource('/user', UsersController::class);
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
